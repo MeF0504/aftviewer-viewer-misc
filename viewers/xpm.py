d3c07ed1cc2a7188066db1fc81c7ec48ad416cfc
Takuro Fujino <fujino.takuro.92@gmail.com>
2026-06-28 16:11:42 +0900

    Small update.

--- viewers/xpm.py
+++ viewers/xpm.py
@@ -1,9 +1,11 @@
 import os
 from logging import getLogger
 from pathlib import Path
+import argparse
 
 from aftviewer import (GLOBAL_CONF, Args, show_image_ndarray, help_template,
                        add_args_imageviewer)
+from pymeflib import xpm_loader
 from pymeflib.xpm_loader import XPMLoader
 logger = getLogger(GLOBAL_CONF.logname)
 if 'numpy' in GLOBAL_CONF.pack_list:
@@ -12,8 +14,10 @@ else:
     use_numpy = False
 
 
-def add_args(parser):
+def add_args(parser: argparse.ArgumentParser):
     add_args_imageviewer(parser)
+    parser.add_argument('--make', help='force make the library.',
+                        action='store_true')
 
 
 def show_help():
@@ -22,6 +26,8 @@ def show_help():
 
 
 def main(fpath: Path, args: Args) -> int:
+    if args.make:
+        xpm_loader._update_lib = True
     xpm = XPMLoader(fpath, logger=logger)
     if use_numpy:
         xpm.xpm_to_ndarray()
